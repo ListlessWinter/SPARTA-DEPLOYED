5334dfb1c72503d384fa6c922eaa0b4f418ee1d6
Vincent <158262786+vdolera@users.noreply.github.com>
2025-12-09 05:23:38 +0800

    Test

--- backend/routes/notificationService.js
+++ backend/routes/notificationService.js
@@ -117,7 +117,7 @@ const checkAndSendMatchNotifications = async () => {
 };
 
 const start = () => {
-  // Change for different time, (0 * * * *) is every hr and all * is every min
+  // Change for different time, (* * * * *) is every hr and all * is every min
   cron.schedule('0 6 * * *', checkAndSendMatchNotifications, {
     scheduled: true,
     timezone: "Asia/Manila", 
